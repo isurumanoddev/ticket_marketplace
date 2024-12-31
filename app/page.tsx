a970c2b59a88cb1e90448c3654cd403c120b40f4
isurumanoddev <isurumanod99@gmail.com>
2024-12-31 20:33:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our docs sdf
           </a>
         </div>
       </main>
